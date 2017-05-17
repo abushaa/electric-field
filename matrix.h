24465fd8a665ec028ee0cb9b9710abaa6b2ff32d
abushaa <alishaabu@mail.ru>
2017-05-17 22:21:02 +0300

    Well, actually class matrix is not my task anymore, and this code probably is useless. Still let's commit changes.

--- matrix.h
+++ matrix.h
@@ -8,9 +8,17 @@
 #endif //ELECTRIC_FIELD_MATRIX_H
 class matrix {
 private:
-    double **a;
+    double a;
     int n, m;
 public:
+    // матрица без элементов
+    matrix ()
+    {
+        a = 0;
+        n = 0;
+        m = 0;
+    }
+
     // получить строку матрицы
     double *getRow(int index) {
         if (index >= 0 && index < n)
@@ -27,5 +35,35 @@ public:
             c[i] = a[i][index];
         return c;
     }
+    // удаление столбцов с index1 по index2
+    matrix eraseColumns (int index1, int index2)
+    {
+        matrix c = matrix (n, m - (index2 - index1 + 1));
+        for (int j = 0; j < index1; ++ j)
+            for (int i = 0; i < n; ++ i)
+                c[i][j] = a[i][j];
+        for (int j = index2 + 1; j < m; ++ j)
+            for (int i = 0; i < n; ++ i)
+                c[i][j - index2 - 1 + index1] = a[i][j];
+        return c;
+    }
+    int *getSize(){
+        int Size;
+        Size = n*m;
+        return Size;
+    }
+    // сколько строк в матрице
+    int *getNumberofStrings() {
+        return n;
+    }
+    // сколько столбцов в матрице
+    int *getNumberofColumns() {
+        return m;
+    }
+    //вывести элемент матрицы
+    double *getElement(int index1, int index2){
+        return
+};
+
 };
 //прочитать матрицу с файла
