2cb1b8db7033b5f84578fd203bdf63233cb3856f
spidervan17 <spidervan@inbox.ru>
2017-05-22 16:22:20 +0300

    Улучшено отображение полей, добавлено считывание с файла

--- fields_themselves.py
+++ fields_themselves.py
@@ -11,33 +11,29 @@ example shows a few features of the stream plot function:
 import numpy as np
 import matplotlib.pyplot as plt
 
+input_txt = open('q.txt', 'r')
+input_data = input_txt.readlines()
+input_txt.close()
 #Напишем
-data = [[1,2,0.20,0.50],[1,10,0.70,0.50]]
 
-Y, X = np.mgrid[0:1:100j, 0:1:100j]
+Y, X = np.mgrid[0:100:10j, 0:100:10j]
 n = 0
 U = 0
 V = 0
-while n < len(data):
-    q = data[n][1]
-    x0 = data[n][2]
-    y0 = data[n][3]
+while n < len(input_data):
+    string = input_data[n].rstrip()
+    data = [float(x) for x in string.rstrip().split()]
+    q = int(data[1])
+    x0 = int(data[2])
+    y0 = int(data[3])
     distance = ((x0-X)**2 + (y0-Y)**2)**0.5
     U += q*(Y-y0)/(distance**3)
     V += q * (X - x0) / (distance ** 3)
     n+=1
-#U = -1 - X**2 + 2*Y
-#V = 1 + 2*X - Y**2
+
 speed = np.sqrt(U*U + V*V)
-
 fig0, ax0 = plt.subplots()
-strm = ax0.streamplot(X, Y, U, V, color=U, linewidth=2, cmap=plt.cm.autumn)
+strm = ax0.streamplot(X, Y, V, U, color=speed, linewidth=2, cmap=plt.cm.autumn)
 fig0.colorbar(strm.lines)
-
-#fig1, (ax1, ax2) = plt.subplots(ncols=2)
-#ax1.streamplot(X, Y, U, V, density=[0.5, 1])
-
-#lw = 5*speed / speed.max()
-#ax2.streamplot(X, Y, U, V, density=0.6, color='k', linewidth=lw)
-
+plt.title('Распределение напряженности электрического поля')
 plt.show()
